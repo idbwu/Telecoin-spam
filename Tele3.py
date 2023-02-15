2191faabeea899330100e87ebe787004fe3e8019
idbwu <125094957+idbwu@users.noreply.github.com>
2023-02-15 23:13:25 +0500

    Add files via upload

---
+++ Tele3.py
@@ -0,0 +1,24 @@
+from pyrogram import Client, filters, types, raw
+import logging
+
+from .. import loader, utils
+
+logger = logging.getLogger(__name__)
+
+@loader.tds
+class TelecoinMod(loader.Module):
+	strings = {"name": "Майнер"}
+		
+	@loader.unrestricted
+	async def gotccmd(self, message):
+		@bot.on_message(filters.regex("1"))
+		async def send_messages(bot, message):
+		      for i in range(1):
+		      	await bot.send_message('tele_coinbot', f"⚒ Добывать")
+		      	if i == 2:
+		      		break
+		bot.run()
+		message = await utils.answer(message, "<code>Спам в Telecoin запущен<code>\n@idbwu")
+		
+
+				
